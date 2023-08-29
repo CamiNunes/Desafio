2df50152cd547f9cd10b9156a5afb7f31952f5fe
Camila Nunes <cvn.camila@gmail.com>
2023-08-29 17:54:52 -0300

    criando entidades;

---
+++ crud-clientes.Domain/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud_clientes.Domain
+{
+    public class Cliente
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<Logradouro> Logradouros { get; set; }
+    }
+}
